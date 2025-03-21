641991721ed2880124e55680912b4f111d63223f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-03-21 08:44:54 +0700

    Filtering

---
+++ src/main/java/org/example/sof3061/streamApi/FilteringDemo.java
@@ -0,0 +1,19 @@
+package org.example.sof3061.streamApi;
+
+import java.util.List;
+
+public class FilteringDemo {
+
+    public static void main(String[] args) {
+
+        List<Movie> movies = List.of(
+                new Movie("a", 10),
+                new Movie("b", 20),
+                new Movie("c", 30)
+        );
+
+        movies.stream()
+                .filter(movie -> movie.getLikes() > 10)
+                .forEach(movie -> System.out.println(movie.getTitle()));
+    }
+}
