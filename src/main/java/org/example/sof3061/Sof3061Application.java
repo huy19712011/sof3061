618f44c5ce9f82fc8ea1f423eef6341ea25b36be
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-03-23 11:46:54 +0700

    @PostMapping: add Todo - part 2: using ModelMapper library

--- src/main/java/org/example/sof3061/Sof3061Application.java
+++ src/main/java/org/example/sof3061/Sof3061Application.java
@@ -1,10 +1,18 @@
 package org.example.sof3061;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Sof3061Application {
+
+    @Bean
+    public ModelMapper modelMapper() {
+
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Sof3061Application.class, args);
