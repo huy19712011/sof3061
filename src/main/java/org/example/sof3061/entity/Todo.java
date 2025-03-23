553011f660ee17ddddab7ce5801ec6214410f4b3
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-03-23 10:38:15 +0700

    Update pom.xml (lombok, hibernate logging); application.properties (connecting with SQL tables); creating Todo entity

---
+++ src/main/java/org/example/sof3061/entity/Todo.java
@@ -0,0 +1,29 @@
+package org.example.sof3061.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+}
